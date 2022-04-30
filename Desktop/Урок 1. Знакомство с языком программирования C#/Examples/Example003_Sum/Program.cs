b8962aff1a8d5de3a7696b2390dadb454c68f623
Semen Muzalevskyi <muzalevskyi.sema@mail.ru>
2022-04-30 15:47:26 +0700

    random

---
+++ Desktop/Урок 1. Знакомство с языком программирования C#/Examples/Example003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int nubmerB = new Random().Next(1, 10);
+Console.WriteLine(nubmerB);
+int result = numberA + nubmerB;
+Console.WriteLine(result);
